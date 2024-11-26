aedeb194286f1652c86b89e3285880f05dcfb8a6
Edoardo <edfl.tunzi@gmail.com>
2024-11-26 15:44:51 +0100

    footer component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import MyNav from "./components/MyNav";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import MyNav from "./components/MyNav";
+import MyFooter from "./components/MyFooter";
 
 function App() {
   return (
     <>
       <MyNav title="EpiBooks" />
+      <MyFooter />
     </>
   );
 }
---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,11 @@
+const MyFooter = () => {
+  return (
+    <div className="bg-dark text-white text-center ">
+      <p className="">Footer content</p>
+      <a href="#" className="mb-5">
+        Back to top
+      </a>
+    </div>
+  );
+};
+export default MyFooter;
