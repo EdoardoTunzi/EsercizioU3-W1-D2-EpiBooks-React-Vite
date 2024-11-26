6692ed4f306ee34e0d7ec52e17cf48065d8074ff
Edoardo <edfl.tunzi@gmail.com>
2024-11-26 18:37:23 +0100

    strictMode deleted, little changes on components

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,5 +1,9 @@
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
-import fantasyBooks from "../data/fantasy.json";
+import fantasy from "../data/fantasy.json";
+//import history from "../data/history.json";
+//import horror from "../data/horror.json";
+//import romance from "../data/romance.json";
+//import scifi from "../data/scifi.json";
 
 const AllTheBooks = () => {
   return (
@@ -11,9 +15,9 @@ const AllTheBooks = () => {
         <button className="btn btn-warning fs-5">Romance</button>
         <button className="btn btn-warning fs-5">Sci-fi</button>
       </div>
-      <Row className="justify-content-center gap-2 mb-4">
-        {fantasyBooks.map((book) => (
-          <Col key={book.asin} sm={6} md={4} lg={3} xxl={2}>
+      <Row xs={1} sm={2} md={3} lg={4} xl={5} className="justify-content-center gap-2 mb-4">
+        {fantasy.map((book) => (
+          <Col key={book.asin}>
             <Card className="h-100">
               <Card.Img variant="top" src={book.img} alt={book.title} fluid className="object-fit-cover" style={{ height: "20rem" }} />
               <Card.Body className="d-flex flex-column">
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,7 +2,7 @@ const Welcome = () => {
   return (
     <div className="jumbotron bg-warning text-center p-5 rounded-5 m-4">
       <h1 className="display-4">Welcome on our website</h1>
-      <p className="fs-4">
+      <p className="fs-4 lead">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates neque veritatis quasi? Minima ea enim iusto, quibusdam quos veniam laboriosam
         commodi sint, reprehenderit obcaecati molestiae rem! Id totam aspernatur veniam!
       </p>
