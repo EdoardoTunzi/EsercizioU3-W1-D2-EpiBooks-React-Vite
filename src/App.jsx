b9b06dc03045dadec8404e48643964b3a87a08ed
Edoardo <edfl.tunzi@gmail.com>
2024-11-26 15:53:05 +0100

    welcome component created

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MyNav from "./components/MyNav";
 import MyFooter from "./components/MyFooter";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
     <>
       <MyNav title="EpiBooks" />
+      <Welcome />
       <MyFooter />
     </>
   );
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,12 @@
+const Welcome = () => {
+  return (
+    <div className="jumbotron bg-warning text-center p-5 rounded-5 m-4">
+      <h1 className="display-4">Welcome on our website</h1>
+      <p className="fs-4">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates neque veritatis quasi? Minima ea enim iusto, quibusdam quos veniam laboriosam
+        commodi sint, reprehenderit obcaecati molestiae rem! Id totam aspernatur veniam!
+      </p>
+    </div>
+  );
+};
+export default Welcome;
