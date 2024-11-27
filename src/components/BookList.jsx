448d6ee1dd346a16d4f9d71e47088f364a9d279e
Edoardo <edfl.tunzi@gmail.com>
2024-11-27 16:17:26 +0100

    booklist converted in class, filter on book from searc input query implemented adn working

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,27 +1,41 @@
 import { Col, Container, Form, Row } from "react-bootstrap";
 import SingleBook from "./SingleBook";
+import { Component } from "react";
 
-const BookList = ({ books }) => {
-  return (
-    <Container fluid className="mt-5">
-      {/* <div className="d-flex justify-content-center gap-2 mb-5 fs-3">
-        <button className="btn btn-warning fs-5">Fantasy</button>
-        <button className="btn btn-warning fs-5">History</button>
-        <button className="btn btn-warning fs-5">Horror</button>
-        <button className="btn btn-warning fs-5">Romance</button>
-        <button className="btn btn-warning fs-5">Sci-fi</button>
-      </div>*/}
-      <Form>
-        <Form.Control type="search" placeholder="Search books by title..." className="mb-4" />
-      </Form>
-      <Row xs={1} sm={2} md={3} lg={4} xl={5} className="justify-content-center gap-2 mb-4">
-        {books.map((book) => (
-          <Col key={book.asin}>
-            <SingleBook book={book} />
-          </Col>
-        ))}
-      </Row>
-    </Container>
-  );
-};
+class BookList extends Component {
+  state = {
+    searchQuery: ""
+  };
+
+  handleSearch = (event) => {
+    this.setState({ searchQuery: event.target.value });
+  };
+  render() {
+    const { books } = this.props;
+    const { searchQuery } = this.state;
+    const filteredBooks = books.filter((book) => book.title.toLowerCase().includes(searchQuery.toLowerCase()));
+
+    return (
+      <Container fluid className="mt-5">
+        {/* <div className="d-flex justify-content-center gap-2 mb-5 fs-3">
+            <button className="btn btn-warning fs-5">Fantasy</button>
+            <button className="btn btn-warning fs-5">History</button>
+            <button className="btn btn-warning fs-5">Horror</button>
+            <button className="btn btn-warning fs-5">Romance</button>
+            <button className="btn btn-warning fs-5">Sci-fi</button>
+          </div>*/}
+        <Form>
+          <Form.Control type="search" placeholder="Search books by title..." className="mb-4" value={searchQuery} onChange={this.handleSearch} />
+        </Form>
+        <Row xs={1} sm={2} md={3} lg={4} xl={5} className="justify-content-center gap-2 mb-4">
+          {filteredBooks.map((book) => (
+            <Col key={book.asin}>
+              <SingleBook book={book} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    );
+  }
+}
 export default BookList;
