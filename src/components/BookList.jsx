0af52b85427f061719a423fae28f8998cc935ff9
Edoardo <edfl.tunzi@gmail.com>
2024-12-03 15:50:27 +0100

    BookList component refactor Done

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,55 +1,49 @@
 import { Col, Container, Form, Row } from "react-bootstrap";
 import SingleBook from "./SingleBook";
-import { Component } from "react";
+import { useState } from "react";
 import CommentArea from "./CommentArea";
 
-class BookList extends Component {
-  state = {
+const BookList = ({ books }) => {
+  /*   state = {
     searchQuery: "",
     selectedBookAsin: ""
+  }; */
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedBookAsin, setSelectedBookAsin] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
   };
 
-  handleSearch = (event) => {
-    this.setState({ searchQuery: event.target.value });
+  const handleBookSelection = (book) => {
+    setSelectedBookAsin(book.asin);
   };
 
-  handleBookSelection = (book) => {
-    this.setState({ selectedBookAsin: book.asin });
-  };
-  render() {
-    const { books } = this.props;
-    const { searchQuery, selectedBookAsin } = this.state;
-    const filteredBooks = books.filter((book) => book.title.toLowerCase().includes(searchQuery.toLowerCase()));
+  /* const { books } = this.props;
+    const { searchQuery, selectedBookAsin } = this.state; */
+  const filteredBooks = books.filter((book) => book.title.toLowerCase().includes(searchQuery.toLowerCase()));
 
-    return (
-      <Container fluid className="mt-5">
-        {/* <div className="d-flex justify-content-center gap-2 mb-5 fs-3">
-            <button className="btn btn-warning fs-5">Fantasy</button>
-            <button className="btn btn-warning fs-5">History</button>
-            <button className="btn btn-warning fs-5">Horror</button>
-            <button className="btn btn-warning fs-5">Romance</button>
-            <button className="btn btn-warning fs-5">Sci-fi</button>
-          </div>*/}
-        <Row className="justify-content-center">
-          <Col xs={6}>
-            <Form.Control type="search" placeholder="Search books by title..." className="mb-4" value={searchQuery} onChange={this.handleSearch} />
-          </Col>
-        </Row>
-        <Row>
-          <Col md={9}>
-            <Row xs={1} sm={2} md={3} lg={5} className="justify-content-center gap-2">
-              {filteredBooks.map((book) => (
-                <Col key={book.asin}>
-                  <SingleBook book={book} onBookSelect={this.handleBookSelection} selectedBookAsin={selectedBookAsin} />
-                </Col>
-              ))}
-            </Row>
-          </Col>
+  return (
+    <Container fluid className="mt-5">
+      <Row className="justify-content-center">
+        <Col xs={6}>
+          <Form.Control type="search" placeholder="Search books by title..." className="mb-4" value={searchQuery} onChange={handleSearch} />
+        </Col>
+      </Row>
+      <Row>
+        <Col md={9}>
+          <Row xs={1} sm={2} md={3} lg={5} className="justify-content-center gap-2">
+            {filteredBooks.map((book) => (
+              <Col key={book.asin}>
+                <SingleBook book={book} onBookSelect={handleBookSelection} selectedBookAsin={selectedBookAsin} />
+              </Col>
+            ))}
+          </Row>
+        </Col>
 
-          <Col md={3}>{selectedBookAsin ? <CommentArea bookID={selectedBookAsin} /> : <div>Select a book to read reviews</div>}</Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+        <Col md={3}>{selectedBookAsin ? <CommentArea bookID={selectedBookAsin} /> : <div>Select a book to read reviews</div>}</Col>
+      </Row>
+    </Container>
+  );
+};
 export default BookList;
