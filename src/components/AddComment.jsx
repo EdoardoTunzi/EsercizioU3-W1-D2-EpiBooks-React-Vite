127015f49f58bab8bfe8de981b816c0cb7514761
Edoardo <edfl.tunzi@gmail.com>
2024-12-03 15:37:07 +0100

    addComment component refactor done

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,19 +1,23 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
-class AddComment extends Component {
-  state = {
+const AddComment = ({ bookID, onCommentAdded }) => {
+  /*   state = {
     comment: "",
     rate: 1
   };
+ */
 
-  handleSubmit = async (e) => {
+  const [comment, setComment] = useState("");
+  const [rate, setRate] = useState(1);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const newComment = {
-      comment: this.state.comment,
-      rate: this.state.rate,
-      elementId: this.props.bookID
+      comment: comment,
+      rate: rate,
+      elementId: bookID
     };
     console.log(newComment);
     try {
@@ -27,41 +31,35 @@ class AddComment extends Component {
         }
       });
       if (response.ok) {
-        this.setState({ comment: "", rate: 1 });
-        this.props.onCommentAdded();
+        /* this.setState({ comment: "", rate: 1 }); */
+        setComment("");
+        setRate(1);
+        onCommentAdded();
       }
     } catch (error) {
       console.log("Error posting comment:", error);
     }
   };
-  render() {
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Group className="mb-1" controlId="review-text">
-          <Form.Control
-            type="textarea"
-            placeholder="Write your comment"
-            value={this.state.comment}
-            onChange={(e) => this.setState({ ...this.state, comment: e.target.value })}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="review-rating">
-          <Form.Select value={this.state.rate} onChange={(e) => this.setState({ ...this.state, rate: e.target.value })} required>
-            <option value="1">⭐️</option>
-            <option value="2">⭐️⭐️</option>
-            <option value="3">⭐️⭐️⭐️</option>
-            <option value="4">⭐️⭐️⭐️⭐️</option>
-            <option value="5">⭐️⭐️⭐️⭐️⭐️</option>
-          </Form.Select>
-        </Form.Group>
-        <div className="d-flex justify-content-center">
-          <Button variant="warning" className="mb-3" type="submit">
-            Add comment
-          </Button>
-        </div>
-      </Form>
-    );
-  }
-}
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-1" controlId="review-text">
+        <Form.Control as="textarea" placeholder="Write your comment" value={comment} onChange={(e) => setComment(e.target.value)} required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="review-rating">
+        <Form.Select value={rate} onChange={(e) => setRate(e.target.value)} required>
+          <option value="1">⭐️</option>
+          <option value="2">⭐️⭐️</option>
+          <option value="3">⭐️⭐️⭐️</option>
+          <option value="4">⭐️⭐️⭐️⭐️</option>
+          <option value="5">⭐️⭐️⭐️⭐️⭐️</option>
+        </Form.Select>
+      </Form.Group>
+      <div className="d-flex justify-content-center">
+        <Button variant="warning" className="mb-3" type="submit">
+          Add comment
+        </Button>
+      </div>
+    </Form>
+  );
+};
 export default AddComment;
