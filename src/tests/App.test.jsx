907df8e8b2825840a2af3ca1ac05e29b8447e210
Edoardo <edfl.tunzi@gmail.com>
2024-12-05 16:22:11 +0100

    jest true added, test 1,2, 3 done

---
+++ src/tests/App.test.jsx
@@ -0,0 +1,52 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import App from "../App";
+import Welcome from "../components/Welcome";
+import BookList from "../components/BookList";
+import fantasy from "../data/fantasy.json";
+/* describe('App', () => {
+  it('renders the App component', () => {
+    render(<App />)
+    
+    screen.debug(); // prints out the jsx in the App component unto the command line
+  })
+}) */
+
+//TEST 1
+//questo describe è la sezione dei test su Welcome
+describe("verifies Welcome functionality", () => {
+  // singolo test e descrizione
+  it("checks if welcome is mounted correctly", () => {
+    //definisco l'elemento su cui fare il test
+    render(<Welcome />);
+    //mi prendo nel dom virtuale l'elemento da valutare
+    const heading = screen.getByText(/Welcome on our website/i);
+    screen.debug(heading);
+    //definisco la mia aspettativa, la condizione da aspettarsi come risultato del test
+    expect(heading).toBeInTheDocument();
+  });
+});
+//TEST 2
+describe("verifies Booklist contains ....", () => {
+  it("check cards number in booklist ", async () => {
+    render(<App />);
+    const card = await screen.queryAllByRole("img");
+    screen.debug(card);
+    expect(card.length).toBe(fantasy.length);
+  });
+});
+
+//test 4
+describe("verifies functionality of Booklist", () => {
+  it("check if CommentArea is rendered correctly", async () => {
+    render(<App />);
+    const inputField = screen.getByPlaceholderText(/Search books by title/i);
+    fireEvent.change(inputField, { target: { value: "dragon" } });
+    const filteredElements = await screen.findAllByRole("img");
+
+    expect(filteredElements).toHaveLength(8);
+  });
+});
+
+//test 5 Verifica che, cliccando su un libro, il suo bordo cambi colore.
+describe("");
